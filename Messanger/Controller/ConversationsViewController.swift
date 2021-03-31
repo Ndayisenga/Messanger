6394d9eb53bb9852467a69d0711e88507e74de78
Ndayisenga Jean Claude <cloude056@yahoo.fr>
2021-03-31 14:14:45 +0200

    Messanger

---
+++ Messanger/Controller/ConversationsViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Messanger
+//
+//  Created by Ndayisenga Jean Claude on 28/03/2021.
+//
+
+import UIKit
+
+class ConversationsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let isLoggedIn = UserDefaults.standard.bool(forKey: "logged_in")
+        if !isLoggedIn {
+            let vc = LoginViewController()
+            let nav = UINavigationController(rootViewController: vc)
+            nav.modalPresentationStyle = .fullScreen
+            present(nav, animated: false)
+        }
+    }
+
+}
+
+
