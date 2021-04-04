fbbf63d9f631cc1eae5c5877c28a85147f8da179
Ndayisenga Jean Claude <cloude056@yahoo.fr>
2021-04-05 00:17:56 +0200

    Messanger V5F Database setup

--- Messanger/Resources/DatabaseManager.swift
+++ Messanger/Resources/DatabaseManager.swift
@@ -22,7 +22,9 @@ extension DatabaseManager {
     public func userExists(with email: String,
                            completion: @escaping ((Bool) -> Void)) {
         
-        database.child(email).observeSingleEvent(of: .value, with: { snapshot in
+        var safeEmail = email.replacingOccurrences(of: ".", with: "-")
+        safeEmail = safeEmail.replacingOccurrences(of: "@", with: "-")
+        database.child(safeEmail).observeSingleEvent(of: .value, with: { snapshot in
             guard snapshot.value as? String != nil else {
                 completion(false)
                 return
@@ -34,7 +36,7 @@ extension DatabaseManager {
     /// Inserts new user to database
     
     public func insertUser(with user: ChatAppUser) {
-        database.child(user.emailAddress).setValue([
+        database.child(user.safeEmail).setValue([
             "first_name": user.firstName,
             "last_name": user.lastName
         ])
@@ -45,5 +47,14 @@ struct ChatAppUser {
     let firstName: String
     let lastName: String
     let emailAddress: String
+    
+    
+    var safeEmail: String {
+        var safeEmail = emailAddress.replacingOccurrences(of: ".", with: "-")
+        safeEmail = safeEmail.replacingOccurrences(of: "@", with: "-")
+        return safeEmail
+    }
+    
+    
     // let profilePictureUrl: String
 }
